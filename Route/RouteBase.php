b3d7be09f3b551a0d86956e66c7e3d59dd3cf376
HanHui <zhujingfa@gmail.com>
2013-09-23 23:05:37 +0800

    采用抽象方法定义路由共同接口

--- Route/RouteBase.php
+++ Route/RouteBase.php
@@ -4,11 +4,16 @@ namespace HuiLib\Route;
 /**
  * 路由模块基类
  *
+ * 二级目录、短链路由、子域名、拓展域名路由共用接口
+ * 采用抽象方法定义共同接口
+ * 
  * @author 祝景法
  * @since 2013/09/15
  */
-class RouteBase
+abstract class RouteBase
 {
 	
-	
+	abstract function route(){
+		
+	}
 }
