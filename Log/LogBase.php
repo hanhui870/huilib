7f29baed13b590330b3a250b853f5d0bc47a3178
HanHui <zhujingfa@gmail.com>
2013-10-18 23:54:59 +0800

    LogBase 日志服务 结构

--- Log/LogBase.php
+++ Log/LogBase.php
@@ -1,1 +1,16 @@
 <?php
+namespace HuiLib\Log;
+
+/**
+ * File基础类
+ *
+ * @author 祝景法
+ * @since 2013/10/18
+ */
+class LogBase
+{
+	protected function __construct($config)
+	{
+	
+	}
+}
--- Log/Storage/File.php
+++ Log/Storage/File.php
@@ -1,5 +1,5 @@
 <?php
-namespace HuiLib\Cache\Storage;
+namespace HuiLib\Log\Storage;
 
 /**
  * File基础类
@@ -7,7 +7,7 @@ namespace HuiLib\Cache\Storage;
  * @author 祝景法
  * @since 2013/09/15
  */
-class File extends \HuiLib\Cache\CacheBase
+class File extends \HuiLib\Log\LogBase
 {
 	protected function __construct($config)
 	{
