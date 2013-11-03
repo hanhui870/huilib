da2d7dcf45311c385fc5a45f90bc99a51fa12bfb
HanHui <zhujingfa@gmail.com>
2013-11-03 21:48:10 +0800

    UA模块优化

--- Helper/UserAgent.php
+++ Helper/UserAgent.php
@@ -10,17 +10,30 @@ namespace HuiLib\Helper;
 class UserAgent
 {
 	//UA副本
-	private $userAgent='';
+	private $userAgent = '';
+	//broswer
+	private $broswer = '';
 	private static $instance;
-
-	private function __construct(){
-		$this->userAgent=Param::server('HTTP_USER_AGENT', Param::TYPE_STRING);
-	}
 	
+	const BROWSER_IE = 'IE';
+	const BROWSER_CHROME = 'Chrome';
+	const BROWSER_SOGOU = 'SogouBrowser';
+	const BROWSER_360SAFE = '360Safe';
+	const BROWSER_OPERA = 'Opera';
+	const BROWSER_SAFARI = 'Safari';
+	const BROWSER_FIREFOX='Firefox';
+
+	private function __construct()
+	{
+		$this->userAgent = Param::server ( 'HTTP_USER_AGENT', Param::TYPE_STRING );
+		$this->initBroswer ();
+	}
+
 	/**
 	 * 是否是爬虫
 	 */
-	public function isRobot() {
+	public function isRobot()
+	{
 		$kw_spiders = 'Bot|Spider|Crawl|search';
 		$kw_browsers = 'MSIE|360SE|Chrome|MetaSr|Safari|Opera|Mozilla';
 		if (strpos ( $this->userAgent, 'http://' ) === FALSE && preg_match ( "/($kw_browsers)/i", $this->userAgent )) {
@@ -33,12 +46,92 @@ class UserAgent
 		
 		return $isRobot;
 	}
-	
+
+	/**
+	 * 处理useragent 设置当前浏览器
+	 */
+	private function initBroswer()
+	{
+		//设置UA 大小写不敏感
+		if (strexists ( $this->userAgent, '360SE' )) { //360浏览器
+			$this->broswer = self::BROWSER_360SAFE;
+		} elseif (strexists ( $this->userAgent, 'MetaSr' )) { //搜狗浏览器3
+			$this->broswer = self::BROWSER_SOGOU;
+		} elseif (strexists ( $this->userAgent, 'MSIE' )) { //IE浏览器
+			$this->broswer = self::BROWSER_IE;
+		} elseif (strexists ( $this->userAgent, 'Chrome' )) { //Chrome浏览器
+			$this->broswer = self::BROWSER_CHROME;
+		} elseif (strexists ( $this->userAgent, 'Googlebot' )) {
+			$this->broswer = 'Googlebot';
+		} elseif (strexists ( $this->userAgent, 'Baiduspider' )) {
+			$this->broswer = 'Baiduspider';
+		} elseif (strexists ( $this->userAgent, 'Sogou' )) {
+			$this->broswer = 'Sogouspider';
+		} elseif (strexists ( $this->userAgent, 'Opera' )) { //Opera浏览器
+			$this->broswer = self::BROWSER_OPERA;
+		} elseif (strexists ( $this->userAgent, 'Safari' )) { //Safari浏览器 包括iPhone
+			$this->broswer = self::BROWSER_SAFARI;
+		} elseif (strexists ( $this->userAgent, 'Firefox' )) { //Firefox
+			$this->broswer = self::BROWSER_FIREFOX;
+		} elseif (strexists ( $this->userAgent, 'Sosospider' )) {
+			$this->broswer = 'Sosospider';
+		} elseif (strexists ( $this->userAgent, 'YoudaoBot' )) {
+			$this->broswer = 'YoudaoBot';
+		} elseif (strexists ( $this->userAgent, 'bingbot' )) {
+			$this->broswer = 'Bingbot';
+		} else {
+			$this->broswer = 'Other';
+		}
+		
+		return true;
+	}
+
+	/**
+	 * 处理useragent 设置当前浏览器
+	 */
+	public function getBroswer()
+	{
+		return $this->broswer;
+	}
+
 	/**
 	 * 是否IE浏览器
 	 */
-	public function isIE() {
-		return preg_match('/MSIE/', $this->userAgent);
+	public function isIE()
+	{
+		return $this->broswer==self::BROWSER_IE;
+	}
+	
+	/**
+	 * 是否360浏览器
+	 */
+	public function is360()
+	{
+		return $this->broswer==self::BROWSER_360SAFE;
+	}
+	
+	/**
+	 * 是否Chrome浏览器
+	 */
+	public function isChrome()
+	{
+		return $this->broswer==self::BROWSER_CHROME;
+	}
+
+	/**
+	 * 是否Safari浏览器
+	 */
+	public function isSafari()
+	{
+		return $this->broswer==self::BROWSER_SAFARI;
+	}
+	
+	/**
+	 * 是否Firefox浏览器
+	 */
+	public function isFirefox()
+	{
+		return $this->broswer==self::BROWSER_FIREFOX;
 	}
 	
 	/**
