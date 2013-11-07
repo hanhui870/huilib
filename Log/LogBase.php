ac77ec11ea03c4a0894f2904bfdf336e9cbc0a9d
HanHui <zhujingfa@gmail.com>
2013-11-07 22:09:58 +0800

    准备开发Log相关库

--- Log/LogBase.php
+++ Log/LogBase.php
@@ -9,8 +9,35 @@ namespace HuiLib\Log;
  */
 class LogBase
 {
+	/**
+	 * PHP语言相关错误
+	 */
+	const TYPE_PHPERROR='PHPError';
+	
+	/**
+	 * 运行时错误
+	 */
+	const TYPE_RUNTIME='Runtime';
+	
+	/**
+	 * DAEMON执行的相关人物
+	 */
+	const TYPE_DAEMON='Daemon';
+	
+	/**
+	 * 数据库相关人物
+	 */
+	const TYPE_DBERROR='DBError';
+	
+	/**
+	 * 用户行为相关人物
+	 */
+	const TYPE_USERERROR='UserError';
+	
 	protected function __construct($config)
 	{
 	
 	}
+	
+	
 }
