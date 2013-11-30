bde44488eaae4fdcd1e2e66313688bda1f3de140
HanHui <zhujingfa@gmail.com>
2013-11-30 11:17:50 +0800

    自动生成用户自定义字段

---
+++ Tool/Utility/Table/User.php
@@ -0,0 +1,13 @@
+<?php
+namespace HuiLib\Tool\Utility\Table;
+
+/**
+ * 用户表字段获取模型
+ *
+ * @author 祝景法
+ * @since 2013/11/30
+ */
+class User extends \HuiLib\Tool\Utility\TableField 
+{
+	const TABLE='user';
+}
--- Tool/Utility/TableField.php
+++ Tool/Utility/TableField.php
@@ -1,6 +1,10 @@
 <?php
 namespace HuiLib\Tool\Utility;
 
+use HuiLib\Db\DbBase;
+use HuiLib\Error\Exception;
+use HuiLib\Helper\String;
+
 /**
  * 工具集框架基础
  *
@@ -10,7 +14,31 @@ namespace HuiLib\Tool\Utility;
 class TableField extends \HuiLib\Tool\ToolBase
 {
 	public function run(){
+		if (!defined('static::TABLE')) {
+			throw new Exception('Tool class table name has not been set.');
+		}
 		
+		$db=DbBase::createMaster();
+		$fieldList=$db->getConnection()->query('describe '.static::TABLE)->fetchAll(\Pdo::FETCH_ASSOC);
+		
+		$result=array();
+		foreach ($fieldList as $field)
+		{
+			if (!empty($field['Default'])) {
+				$result[$field['Field']]=$field['Default'];
+			}else{
+				if (String::exist($field['Type'], 'int')) {
+					$result[$field['Field']]=0;
+				}elseif (String::exist($field['Type'], 'varchar')) {
+					$result[$field['Field']]='';
+				}else{
+					$result[$field['Field']]='';
+				}
+			}
+		}
+		
+		//print_r($fieldList);
+		var_export($result);
 	}
 	
 	protected static function className(){
