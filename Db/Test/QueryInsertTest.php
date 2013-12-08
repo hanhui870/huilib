a3ae4680f9271e72f9deb657aace4d4163186de7
HanHui <zhujingfa@gmail.com>
2013-12-08 12:36:35 +0800

    修改键名避免误会 多行dup插入

--- Db/Test/QueryInsertTest.php
+++ Db/Test/QueryInsertTest.php
@@ -10,7 +10,7 @@ namespace HuiLib\Db\Test;
 class QueryInsertTest extends \HuiLib\Test\TestBase
 {
 	public function run(){
-		$this->testKvInsert();
+		$this->testDupValues();
 	}
 	
 	/**
@@ -45,6 +45,17 @@ class QueryInsertTest extends \HuiLib\Test\TestBase
 		//insert into test set field1='fvalue1', field2='fvalue2' on duplicate key update field1='newfvalue1', num=num+1 ;
 		$insert=\HuiLib\Db\Query::Insert()->table('test')->enableDuplicate(true)->fields(array('field1','field2'))->dupFields(array('field1', 'num'))
 		->values(array('fvalue1', 'fvalue2'), array('field2'=>'newfvalue1', array('plain'=>'num=num+1')));
+		//$insert->query();
+		echo $insert->toString();
+	}
+	
+	/**
+	 * 测试多行
+	 */
+	private function testDupValues(){
+		//insert into test set field1='fvalue1', field2='fvalue2' on duplicate key update field1='newfvalue1', num=num+1 ;
+		$insert=\HuiLib\Db\Query::Insert()->table('test')->enableDuplicate(true)->fields(array('field1','field2'))->dupFields(array('field1', 'num'))
+		->values(array('fvalue1', 'fvalue2'), array('field2'=>'newfvalue1', array('plain'=>'num=num+1')))->values(array('fvalue11', 'fvalue22'));
 		//$insert->query();
 		echo $insert->toString();
 	}
