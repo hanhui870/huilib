85760cd0c02b27c643e167f52f44ec3bec0bb9a2
HanHui <zhujingfa@gmail.com>
2014-02-27 22:10:22 +0800

    路由基础文档规范、整理：目录层路由、子域名、拓展域名路由共用接口

--- Route/RouteBase.php
+++ Route/RouteBase.php
@@ -4,11 +4,21 @@ namespace HuiLib\Route;
 /**
  * 路由模块基类
  *
- * 二级目录、短链路由、子域名、拓展域名路由共用接口
+ * 目录层路由、子域名、拓展域名路由共用接口
  * 采用抽象方法定义共同接口
  * 
- * 二级目录短链在Package层处理，在RequestBase类触发 如/zjgs
- * 三级目录短链在Controller成处理，在Controller类触发 如/discuss/how-to-enjoy-zjgsdx /topic/2
+ * 包层短链在Package层处理，在RequestBase/urlRoute触发 如
+ *      /2 => user/view/index
+ *      /hanhui => user/view/index
+ *      /zjgs => topic/view/index
+ * 控制器层短链在Controller层处理，在RequestBase/loadController触发 如
+ *      /discuss/how-to-enjoy-zjgsdx => discuss/view/index?id
+ *      /topic/2 => topic/view/index?id
+ *      /zjgs/thread => topic/view/thread?id
+ *      /hanhui/thread user/view/thread?page=1
+ * 动作层短链在Action层处理，在Controller/dispatch触发，如
+ *      /discuss/2/log => discuss/view/log
+ *      /hanhui/thread/2 => user/view/thread?page=2
  * 
  * @author 祝景法
  * @since 2013/09/15
