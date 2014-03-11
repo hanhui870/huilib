a5cde1cc3b8a8754643b64df9c17a3d262a49082
Bruce <bruce@duomai.com>
2014-03-11 20:47:37 +0800

    TODO 多语言复数翻译接口

--- Lang/Readme.php
+++ Lang/Readme.php
@@ -9,10 +9,14 @@
  * 适配器接口：
  * 		translate($token)：请求翻译一个字串
  * 		translate($token, $param1, $param2, ...)：请求翻译一个字串，并且支持sprintf格式变量解析
+ * 
+ *     复数翻译Zend接口
+ *     $translate->plural("common.hour","common.hours",2);
  */
 
 //快速获取一个Lang翻译器实例
 $lang=\HuiLib\Lang\LangBase::getHuiLibLang();
 
-//请求一个翻译
+//请求一个翻译 
+//多个参数，如果少个会怎么样？
 $lang->translate('HuiLib.lang.test', '芸临网', 'hanhui', 1000, 3.55000);
