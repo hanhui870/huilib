c66fcd3b81a7c0d099b108b3692dd540aa708b2f
HanHui <zhujingfa@gmail.com>
2014-03-23 13:15:09 +0800

    测试Mysql事务

--- Db/Test/PdoTest.php
+++ Db/Test/PdoTest.php
@@ -10,12 +10,37 @@ namespace HuiLib\Db\Test;
 class PdoTest extends \HuiLib\Test\TestBase
 {
 	public function run(){
-		$this->testMysql();
+		$this->testTransaction();
 	}
 	
 	private function testMysql(){
 		$re=\HuiLib\Db\DbBase::createMaster()->getConnection()->query("select count(*) from test.test");
 		var_dump($re->fetchAll());
+	}
+	
+	private function testTransaction()
+	{
+	    $db=\HuiLib\Db\DbBase::createMaster()->getConnection();
+	    
+	    $time=time();
+	    var_dump($time%2);
+	    
+	    try {
+	        $db->beginTransaction();
+	       
+	        $db->query("update test.test set field1='$time' where id=2 ");
+	        
+	        //! 优先级高于 %
+	        if (!($time%2)) {
+	            throw new \Exception('error');
+	        }
+	        
+	        $db->commit();
+	    }catch (\Exception $e){
+	        $db->rollBack();
+	        
+	        echo 'error exception, time:'.$time .' e:'.$e->getMessage();
+	    }
 	}
 
 	protected static function className(){
