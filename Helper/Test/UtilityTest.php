e3511086a131ea58fbcb4378e8a46e8b6f8dc010
HanHui <zhujingfa@gmail.com>
2014-03-27 23:12:08 +0800

    Utility类添加UUID函数

---
+++ Helper/Test/UtilityTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace HuiLib\Helper\Test;
+
+use HuiLib\Helper\Utility;
+/**
+ * 参数类测试用例
+ *
+ * @author 祝景法
+ * @since 2013/09/24
+ */
+class UtilityTest extends \HuiLib\Test\TestBase
+{
+	public function run(){
+		$this->test();
+	}
+	
+	private function test(){
+		echo Utility::genUuid();
+	}
+
+	protected static function className(){
+		return __CLASS__;
+	}
+}
--- Helper/Utility.php
+++ Helper/Utility.php
@@ -152,4 +152,46 @@ class Utility
 	
 		return \HuiLib\Helper\String::substr( $password, 0, 2 ) . str_repeat ( '*', $length > 10 ? 6 : $length - 4 ) . \HuiLib\Helper\String::substr ( $password, - 2, 2 );
 	}
+	
+	/**
+	 * 生成UUID 
+	 * 
+	 * 来自http://stackoverflow.com/questions/2040240
+	 */
+	public static function genUuid() {
+	    $uuid = array(
+	            'time_low'  => 0,
+	            'time_mid'  => 0,
+	            'time_hi'  => 0,
+	            'clock_seq_hi' => 0,
+	            'clock_seq_low' => 0,
+	            'node'   => array()
+	    );
+	
+	    $uuid['time_low'] = mt_rand(0, 0xffff) + (mt_rand(0, 0xffff) << 16);
+	    $uuid['time_mid'] = mt_rand(0, 0xffff);
+	    $uuid['time_hi'] = (4 << 12) | (mt_rand(0, 0x1000));
+	    $uuid['clock_seq_hi'] = (1 << 7) | (mt_rand(0, 128));
+	    $uuid['clock_seq_low'] = mt_rand(0, 255);
+	
+	    for ($i = 0; $i < 6; $i++) {
+	        $uuid['node'][$i] = mt_rand(0, 255);
+	    }
+	
+	    $uuid = sprintf('%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x',
+	            $uuid['time_low'],
+	            $uuid['time_mid'],
+	            $uuid['time_hi'],
+	            $uuid['clock_seq_hi'],
+	            $uuid['clock_seq_low'],
+	            $uuid['node'][0],
+	            $uuid['node'][1],
+	            $uuid['node'][2],
+	            $uuid['node'][3],
+	            $uuid['node'][4],
+	            $uuid['node'][5]
+	    );
+	
+	    return $uuid;
+	}
 }
