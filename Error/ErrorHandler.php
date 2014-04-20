66633f859161a4e5504fcd6b154783c3148d73e5
HanHui <zhujingfa@gmail.com>
2014-04-20 22:34:43 +0800

    异常处理

--- Error/ErrorHandler.php
+++ Error/ErrorHandler.php
@@ -12,19 +12,37 @@ use HuiLib\App\Front;
 class ErrorHandler
 {
 	public static function errorHandle(){
-	
+	    //do not  need display, just log
 	}
 	
 	public static function exceptionHandle(){
 		if (Front::getInstance()->getController()) {
-		    Front::getInstance()->getController()->error();
+		    Front::getInstance()->getController()->exception();
 		}else{
 		    $view = new \HuiLib\App\View ();
 		    $view->message=Front::getInstance()->getLang()->_('error.page.not.found');
 		    $view->forward=NULL;
 		    $view->timeout=NULL;
-		    $view->app['homepage']='/';
 		    
+		    /**
+		     * 应用信息赋值到前台
+		     */
+		    $app = array ();
+		    //向前台赋值当前程序版本
+		    $version = Front::getInstance()->getAppConfig()->getByKey ( 'app.version' );
+		    $app ['version'] = $version;
+		    
+		    //网站首页
+		    $app ['homepage'] = '/';
+		    $app ['https'] = '';
+		    
+		    //模块路由信息
+		    $request=Front::getInstance()->getRequest();
+		    $app ['package'] = $request->getPackageRouteSeg();
+		    $app ['controller'] = $request->getControllerRouteSeg();
+		    $app ['action'] = $request->getActionRouteSeg();
+		    
+		    $view->assign ( 'app', $app );
 		    $view->assign(Front::getInstance()->getSiteConfig()->getByKey());
 	
 		    $view->render('Common/Message');
