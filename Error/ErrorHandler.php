10f3cedc812b647d3e60ed0103eeaf5b66a5ef5b
HanHui <zhujingfa@gmail.com>
2014-04-23 21:41:05 +0800

    异常错误处理 统一定为到errorhandle 另外根据配置显示异常信息

--- Error/ErrorHandler.php
+++ Error/ErrorHandler.php
@@ -15,37 +15,38 @@ class ErrorHandler
 	    //do not  need display, just log
 	}
 	
-	public static function exceptionHandle(){
-		if (Front::getInstance()->getController()) {
-		    Front::getInstance()->getController()->exception();
-		}else{
-		    $view = new \HuiLib\App\View ();
-		    $view->message=Front::getInstance()->getLang()->_('error.page.not.found');
-		    $view->forward=NULL;
-		    $view->timeout=NULL;
-		    
-		    /**
-		     * 应用信息赋值到前台
-		     */
-		    $app = array ();
-		    //向前台赋值当前程序版本
-		    $version = Front::getInstance()->getAppConfig()->getByKey ( 'app.version' );
-		    $app ['version'] = $version;
-		    
-		    //网站首页
-		    $app ['homepage'] = '/';
-		    $app ['https'] = '';
-		    
-		    //模块路由信息
-		    $request=Front::getInstance()->getRequest();
-		    $app ['package'] = $request->getPackageRouteSeg();
-		    $app ['controller'] = $request->getControllerRouteSeg();
-		    $app ['action'] = $request->getActionRouteSeg();
-		    
-		    $view->assign ( 'app', $app );
-		    $view->assign(Front::getInstance()->getSiteConfig()->getByKey());
-	
-		    $view->render('Common/Message');
-		}
+	public static function exceptionHandle($exception)
+	{
+	    $view = new \HuiLib\App\View ();
+	    $view->message=Front::getInstance()->getLang()->_('error.page.not.found');
+	    if (ini_get('display_errors')) {
+	        $view->message.='<pre>'.var_export($exception, true).'</pre>';
+	    }
+	    $view->forward=NULL;
+	    $view->timeout=NULL;
+	    
+	    /**
+	     * 应用信息赋值到前台
+	     */
+	    $app = array ();
+	    //向前台赋值当前程序版本
+	    $version = Front::getInstance()->getAppConfig()->getByKey ( 'app.version' );
+	    $app ['version'] = $version;
+	    
+	    //网站首页
+	    $app ['homepage'] = '/';
+	    $app ['https'] = '';
+	    
+	    //模块路由信息
+	    $request=Front::getInstance()->getRequest();
+	    $app ['package'] = $request->getPackageRouteSeg();
+	    $app ['controller'] = $request->getControllerRouteSeg();
+	    $app ['action'] = $request->getActionRouteSeg();
+	    
+	    $view->assign ( 'app', $app );
+	    $view->assign(Front::getInstance()->getSiteConfig()->getByKey());
+
+	    $view->render('Common/Message');
+		
 	}
 }
