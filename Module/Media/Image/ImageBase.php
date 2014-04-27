fb05eb25b6d2cc7c7e5fbb0d7878547b32d97c93
HanHui <zhujingfa@gmail.com>
2014-04-27 16:25:57 +0800

    图像处理类 获取gif的图像大小可能bug 已经修复

--- Module/Media/Image/ImageBase.php
+++ Module/Media/Image/ImageBase.php
@@ -11,47 +11,283 @@ use HuiLib\Error\MediaException;
  * @author HanHui
  * @since 2014/04/26
  */
-class ImageBase extends \HuiLib\Module\ModuleBase {
-	/**
+class ImageBase extends \HuiLib\Module\ModuleBase
+{
+    /**
 	 * Imagick对象
 	 * @var \Imagick
 	 */
-	protected $image;
-	
-	// 图像初始高度、宽度信息
-	protected $originalSize = array ();
-	
-	protected $maxWidth = 760;
-	// 最高
-	protected $maxHeight = 8000;
-	
-	// 压缩比例
-	protected $compressionquality = 80;
+    protected $image;
+    
+    // 图像初始高度、宽度信息
+    protected $originalSize = array ();
+    
+    // 原图像保存文件大小
+    protected $originalFileSize = array ();
+    
+    //最宽
+    protected $maxWidth = 800;
+    // 最高
+    protected $maxHeight = 8000;
+    
+    //水印资源
+    protected $waterImage = NULL;
+    
+    // 压缩比例
+    protected $compressionquality = 80;
 
-	/**
+    /**
 	 * 初始化imagick对象
 	 *
 	 * @param $filepath string 文件地址
 	 * @param $isBinary boolean 是否是二进制内容或文件地址
 	 */
-	public function __construct($filepath, $isBinary) {
-		parent::__construct ();
-		
-		$this->image = new \Imagick ();
-		
-		try {
-		    // imagick::readImage 在win下5.3版本居然不能直接用。改为readImageBlob可用。
-		    $this->image->readImageBlob ( $isBinary ? $filepath : file_get_contents ( $filepath ) );
-		} catch ( \ImagickException $e ) {
-		    throw new MediaException($e->getMessage());
-		}
-	}
+    public function __construct($filepath, $isBinary = FALSE)
+    {
+        parent::__construct ();
+        
+        $this->image = new \Imagick ();
+        
+        try {
+            // imagick::readImage 在win下5.3版本居然不能直接用。改为readImageBlob可用。
+            $this->image->readImageBlob ( $isBinary ? $filepath : file_get_contents ( $filepath ) );
+            $this->originalFileSize = $this->image->getimagesize ();
+        } catch ( \ImagickException $e ) {
+            throw new MediaException ( $e->getMessage () );
+        }
+    }
 
-	public function getImageFormat() {
-		return  strtolower ( $this->image->getImageFormat () );
-	}
-	
-	public function getOriginalSize() {
-	    return  $this->image->getimagegeometry ();
-	}
+    /**
+	 * 图像缩放操作
+	 *
+	 * @param int $width 目标缩放宽度
+	 * @param int $height 目标缩放高度
+	 * @param $savepath string 文件存放路径，未提供则修改当前
+	 */
+    function thumb($width, $height, $savepath = NULL)
+    {
+        $format = $this->getImageFormat ();
+        
+        // 小动图加快上传
+        if (! empty ( $this->originalSize ) && $this->originalSize ['width'] <= $width && $this->originalSize ['height'] <= $height) {
+            // 不需要压缩、解析等操作，直接保存。imagick对象已经检测图像有效性。
+            $tmpImage = clone $this->image;
+        } else {
+            if ($format == 'GIF') {
+                $tmpImage = new \Imagick ();
+                $transparent = new \ImagickPixel ( "rgba(0, 0, 0, 0)" ); // 透明色
+                foreach ( $this->image as $frame ) {
+                    $page = $frame->getImagePage ();
+                    $tmp = new \Imagick ();
+                    $tmp->newImage ( $page ['width'], $page ['height'], $transparent, $format );
+                    $tmp->compositeImage ( $frame, \Imagick::COMPOSITE_DEFAULT, $page ['x'], $page ['y'] );
+                    // 规格有调整的时候才缩放
+                    if (empty ( $this->originalSize ) || $this->originalSize ['width'] > $width && $this->originalSize ['height'] > $height) {
+                        $tmp->thumbnailImage ( $width, $height );
+                    }
+                    
+                    $tmpImage->addImage ( $tmp );
+                    $tmpImage->setImagePage ( $tmp->getImageWidth (), $tmp->getImageHeight (), 0, 0 );
+                    $tmpImage->setImageDelay ( $frame->getImageDelay () );
+                    $tmpImage->setImageDispose ( $frame->getImageDispose () );
+                }
+            } else {
+                $tmpImage = new \Imagick ();
+                $tmpImage->addimage ( $this->image );
+                
+                $tmpImage->thumbnailImage ( $width, $height );
+            }
+        }
+        
+        if ($savepath == NULL) {
+            //缩放当前
+            $this->image = $tmpImage;
+        } else {
+            //写入储存
+            $tmpImage->coalesceImages ();
+            $tmpImage->setImageFormat ( $format );
+            $tmpImage->setImageCompressionQuality ( $this->compressionquality );
+            $tmpImage->writeimages ( $savepath, 1 );
+        }
+        
+        return $this;
+    }
+
+    /**
+	 * 截取图片
+	 * 
+	 * @param int $x 开始坐标x
+	 * @param int $y 坐标y
+	 * @param int $width
+	 * @param int $height
+	 * @param string $savepath 文件存放路径，未提供则修改当前
+	 */
+    public function crop($x, $y, $width, $height, $savepath = NULL)
+    {
+        $format = $this->getImageFormat ();
+        
+        if ($format == 'GIF') {
+            $transparent = new \ImagickPixel ( "rgba(0, 0, 0, 0)" ); // 透明色
+            $tmpImage = new \Imagick ();
+            foreach ( $this->image as $frame ) {
+                $page = $frame->getImagePage ();
+                $tmp = new \Imagick ();
+                $tmp->newImage ( $page ['width'], $page ['height'], $transparent, $format );
+                $tmp->compositeImage ( $frame, \Imagick::COMPOSITE_DEFAULT, $page ['x'], $page ['y'] );
+                $tmp->cropimage ( $width, $height, $x, $y );
+                
+                $tmpImage->addImage ( $tmp );
+                $tmpImage->setImagePage ( $tmp->getImageWidth (), $tmp->getImageHeight (), 0, 0 );
+                $tmpImage->setImageDelay ( $frame->getImageDelay () );
+                $tmpImage->setImageDispose ( $frame->getImageDispose () );
+            }
+        } else {
+            $tmpImage = new \Imagick ();
+            $tmpImage->addimage ( $this->image );
+            
+            $tmpImage->cropimage ( $width, $height, $x, $y );
+        }
+        
+        if ($savepath == NULL) {
+            //缩放当前
+            $this->image = $tmpImage;
+        } else {
+            //写入储存
+            $tmpImage->coalesceImages ();
+            $tmpImage->setImageFormat ( $format );
+            $tmpImage->setImageCompressionQuality ( $this->compressionquality );
+            $tmpImage->writeimages ( $savepath, 1 );
+        }
+        
+        return $this;
+    }
+
+    /**
+	 * 截取图片
+	 *
+	 * @param int $x 开始坐标x
+	 * @param int $y 坐标y
+	 * @param int $width
+	 * @param int $height
+	 * @param string $savepath 文件存放路径，未提供则修改当前
+	 */
+    public function watchmark($x, $y, $savepath = NULL)
+    {
+        if (! $this->waterImage instanceof \Imagick) {
+            throw new MediaException ( 'Watermark source is invalid.' );
+        }
+        
+        if ($format == 'GIF') {
+            $format = $this->getImageFormat ();
+            $transparent = new \ImagickPixel ( "rgba(0, 0, 0, 0)" ); // 透明色
+            $tmpImage = new \Imagick ();
+            foreach ( $this->image as $frame ) {
+                $page = $frame->getImagePage ();
+                $tmp = new \Imagick ();
+                $tmp->newImage ( $page ['width'], $page ['height'], $transparent, $format );
+                $tmp->compositeImage ( $frame, \Imagick::COMPOSITE_DEFAULT, $page ['x'], $page ['y'] );
+                $tmp->compositeImage ( $this->waterImage, \Imagick::COMPOSITE_DEFAULT, $x, $y );
+                
+                $tmpImage->addImage ( $tmp );
+                $tmpImage->setImagePage ( $tmp->getImageWidth (), $tmp->getImageHeight (), 0, 0 );
+                $tmpImage->setImageDelay ( $frame->getImageDelay () );
+                $tmpImage->setImageDispose ( $frame->getImageDispose () );
+            }
+        } else {
+            $tmpImage = new \Imagick ();
+            $tmpImage->addimage ( $this->image );
+            
+            $tmpImage->compositeImage ( $this->waterImage, \Imagick::COMPOSITE_DEFAULT, $x, $y );
+        }
+        
+        if ($savepath == NULL) {
+            //缩放当前
+            $this->image = $tmpImage;
+        } else {
+            //写入储存
+            $tmpImage->coalesceImages ();
+            $tmpImage->setImageFormat ( $format );
+            $tmpImage->setImageCompressionQuality ( $this->compressionquality );
+            $tmpImage->writeimages ( $savepath, 1 );
+        }
+        
+        return $this;
+    }
+
+    public function getImageFormat()
+    {
+        $format = $this->image->getImageFormat ();
+        return $format;
+    }
+
+    /**
+	 * 获取原始 宽度 高度
+	 * 
+	 * @return array
+	 */
+    public function getOriginalSize()
+    {
+        if ($this->getImageFormat()=='GIF') {
+            $widths=$heights=array();
+            foreach ($this->image as $frame ){
+                $tmp=$frame->getimagegeometry ();
+                array_push($widths, $tmp ['width']);
+                array_push($heights, $tmp ['height']);
+            }
+            $result=array();
+            $result ['width']=max($widths);
+            $result ['height']=max($heights);
+        }else{
+            $result = $this->image->getimagegeometry ();
+        }
+        if (empty ( $result ['width'] ) || empty ( $result ['height'] )) {
+            throw new MediaException ( 'Failed to getOriginalSize()' );
+        }
+        
+        return $result;
+    }
+
+    public function setMaxWidth($width)
+    {
+        $this->maxWidth = $width;
+        return $this;
+    }
+
+    public function setMaxHeight($height)
+    {
+        $this->maxHeight = $height;
+        return $this;
+    }
+
+    /**
+	 * 设置压缩比例
+	 * 
+	 * @param int $quality 压缩比例 0-100
+	 * @return \HuiLib\Module\Media\Image\ImageBase
+	 */
+    public function setCompressQuality($quality)
+    {
+        if ($quality > 100 || $quality < 0) {
+            throw new MediaException ( 'Invalid compressionquality param.' );
+        }
+        $this->compressionquality = $quality;
+        return $this;
+    }
+
+    /**
+	 * 设置水印图片
+	 *
+	 * @param string $filepath
+	 */
+    public function setWaterImage($filepath, $isBinary = FALSE)
+    {
+        $this->waterImage = new \Imagick ();
+        
+        try {
+            // imagick::readImage 在win下5.3版本居然不能直接用。改为readImageBlob可用。
+            $this->waterImage->readImageBlob ( $isBinary ? $filepath : file_get_contents ( $filepath ) );
+        } catch ( \ImagickException $e ) {
+            throw new MediaException ( $e->getMessage () );
+        }
+    }
 }
--- Module/Media/Test/ImageTest.php
+++ Module/Media/Test/ImageTest.php
@@ -2,6 +2,7 @@
 namespace HuiLib\Module\Media\Test;
 
 use HuiLib\Module\Media\Image\ImageBase;
+use HuiLib\Module\Media\Image\Thumb;
 
 /**
  * 图像测试类
@@ -11,20 +12,118 @@ use HuiLib\Module\Media\Image\ImageBase;
  */
 class ImageTest extends \HuiLib\Test\TestBase
 {
-
     public function run()
     {
-        $this->test ();
+        $this->testThumbOpt ();
     }
 
-    private function test()
+    /**
+     * 修改大小
+     */
+    private function testScale()
     {
         $file=dirname(__FILE__).'/test.jpg';
-        $image=ImageBase::create($file, FALSE);
+        $imageJpg=ImageBase::create($file, FALSE);
+        echo $imageJpg->getImageFormat();
+        $imageJpg->thumb(600, 900, dirname(__FILE__).'/test2.jpg');
         
-        echo $image->getImageFormat();
+        //png常常变得很大容量 保存为gif可以缩小体积
+        $file=dirname(__FILE__).'/test.png';
+        $imagePng=ImageBase::create($file, FALSE);
+        echo $imagePng->getImageFormat();
+        $imagePng->thumb(400, 300, dirname(__FILE__).'/test2.png');
+        
+        $file=dirname(__FILE__).'/test.gif';
+        $imageGif=ImageBase::create($file, FALSE);
+        echo $imageGif->getImageFormat();
+        $imageGif->thumb(500, 250, dirname(__FILE__).'/test2.gif');
         
         //print_r($image->getOriginalSize());
+    }
+    
+    /**
+     * 截取图片
+     */
+    private function testCrop()
+    {
+        $file=dirname(__FILE__).'/test.jpg';
+        $imageJpg=ImageBase::create($file, FALSE);
+        $imageJpg->thumb(800, 1200);
+        //echo $imageJpg->getImageFormat();
+        $imageJpg->crop(50, 50, 400, 400, dirname(__FILE__).'/test2.jpg');
+
+        //png常常变得很大容量 保存为gif可以缩小体积
+        $file=dirname(__FILE__).'/test.png';
+        $imagePng=ImageBase::create($file, FALSE);
+        //echo $imagePng->getImageFormat();
+        $imagePng->crop(50, 50, 400, 400, dirname(__FILE__).'/test2.png');
+    
+        $file=dirname(__FILE__).'/test.gif';
+        $imageGif=ImageBase::create($file, FALSE);
+        //echo $imageGif->getImageFormat();
+        $imageGif->crop(50, 50, 400, 400, dirname(__FILE__).'/test2.gif');
+    
+        //print_r($image->getOriginalSize());
+    }
+    
+    /**
+     * 自动缩放截取图片
+     */
+    private function testThumbCrop()
+    {
+        $file=dirname(__FILE__).'/test.jpg';
+        $imageJpg=Thumb::create($file, FALSE);
+        $imageJpg->thumbByCrop(180, 180, dirname(__FILE__).'/testcrop.jpg');
+        
+        //png常常变得很大容量 保存为gif可以缩小体积
+        $file=dirname(__FILE__).'/test.png';
+        $imagePng=Thumb::create($file, FALSE);
+        $imagePng->thumbByCrop(180, 180, dirname(__FILE__).'/testcrop.png');
+        
+        $file=dirname(__FILE__).'/test.gif';
+        $imageGif=Thumb::create($file, FALSE);
+        $imageGif->thumbByCrop(180, 180, dirname(__FILE__).'/testcrop.gif');
+    }
+    
+    /**
+     * 自动缩放截取图片
+     */
+    private function testThumbOpt()
+    {
+        $file=dirname(__FILE__).'/test.jpg';
+        //$imageJpg=Thumb::create($file, FALSE);
+        //$imageJpg->thumbByMin(600, dirname(__FILE__).'/testMin600.jpg');
+        //$imageJpg->thumbByMax(800, dirname(__FILE__).'/testMax800.jpg');
+        //$imageJpg->thumbByMinWidth(400, dirname(__FILE__).'/testMinWidth400.jpg');
+        //$imageJpg->thumbNormalUpload(dirname(__FILE__).'/testNormalUp.jpg');
+        
+        $file=dirname(__FILE__).'/test.gif';
+        $imageJpg=Thumb::create($file, FALSE);
+        $imageJpg->thumbNormalUpload(dirname(__FILE__).'/testNormalUp.gif');
+    }
+    
+    /**
+     * 添加水印
+     */
+    private function testWatermark()
+    {
+        $file=dirname(__FILE__).'/test.jpg';
+        $imageJpg=Thumb::create($file, FALSE);
+        $imageJpg->thumbNormalUpload();
+        $imageJpg->setWaterImage(dirname(__FILE__).'/watermark.png');
+        $imageJpg->waterLeftTop(dirname(__FILE__).'/testWater.jpg');
+        
+        $file=dirname(__FILE__).'/test.png';
+        $imagePng=Thumb::create($file, FALSE);
+        $imagePng->thumbNormalUpload();
+        $imagePng->setWaterImage(dirname(__FILE__).'/watermark.png');
+        $imagePng->waterLeftTop(dirname(__FILE__).'/testWater.png');
+        
+        $file=dirname(__FILE__).'/test.gif';
+        $imageGif=Thumb::create($file, FALSE);
+        $imageGif->thumbNormalUpload();
+        $imageGif->setWaterImage(dirname(__FILE__).'/watermark.png');
+        $imageGif->waterLeftTop(dirname(__FILE__).'/testWater.gif');
     }
 
     protected static function className()
